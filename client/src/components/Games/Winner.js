6b44a4beb64fb48abbe446d1536ee2c93d341505
jn1-name <jn1@catamount.wcu.edu>
2021-04-15 18:49:16 -0400

    User's stats update with content from Firebase

--- client/src/components/Games/Winner.js
+++ client/src/components/Games/Winner.js
@@ -1,7 +1,53 @@
 import styles from '../../assets/Transitions.module.css'
 import { Link } from 'react-router-dom';
+import { updateWins, getWins, getUID } from '../Socket';
+import { increment, db } from '../../firebase';
+
+
 const Waiting = ({ id, winners }) => {
     
+
+
+const updateStats = async (userID) => {
+    updateWins(userID);
+}
+
+const callWins = async (uID) => {
+    return getWins(uID);
+}
+
+const getUserID = async () => {
+    return getUID();
+}
+
+
+    //Works, gets the specific player's wins
+    async function funct1(name) {
+
+        const ID = await funct2(name)
+
+        alert(ID)
+
+        db.collection("users").doc(ID).update({
+            "stats.wins": increment()
+        });   
+    }
+
+    //Works, gets the specific player's wins
+    async function funct2(name) {
+
+        const refID = await db.collection("usernames").doc(name);  
+        
+        return refID.get().then((snap1) => {
+            if(snap1.exists) {
+                return snap1.get("uid")
+            }
+            else {
+                alert("not found")
+            }
+        });
+    }
+
     return (
         <div className = {styles.bgWinner}>
             <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center" >
@@ -10,6 +56,10 @@ const Waiting = ({ id, winners }) => {
                         <>
                             <p className = "h3">{winner.id === id ? "You win!" : `${winner.id} wins!`}</p>
                             <p className = "h6">Final hand value: {winner.total}</p>
+
+                            {/*Update the user's total games won*/}
+                            {/*updateWins(getUID())*/}
+
                         </>
                     ) : <p className = "h3">Nobody wins!</p>}  
                     <div className = "row mt-4 mx-auto"> 
